b802ac680e29b27a6f4123028cc2874d3053bc57
Dariusz Okonski <darek200180@gmail.com>
2021-08-16 23:02:47 +0100

    initialization

---
+++ src/Hello.jsx
@@ -0,0 +1,16 @@
+import {Component} from "react";
+
+class Hello extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>Hello There</h1>
+                <h1>Hello There</h1>
+                <h1>Hello There</h1>                
+            </div>
+         );
+    }
+}
+ 
+export default Hello;
