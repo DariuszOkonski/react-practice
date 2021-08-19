38111caa5d91c76bacf825d77811818750012231
Dariusz Okonski <darek200180@gmail.com>
2021-08-20 00:17:59 +0100

    finish working on Machine

--- src/slotMachines/Machine.jsx
+++ src/slotMachines/Machine.jsx
@@ -3,11 +3,11 @@ import {Component} from "react";
 class Machine extends Component {
     render() { 
         const {s1, s2, s3} = this.props;
-        let result = s1 === s2 && s2 === s3 ? "You Won" : "You Loose";
+        let isWinner = (s1 === s2) && (s2 === s3);
         return ( 
             <div>
-                <h1>{s1} {s2} {s3}</h1>
-                <p>{result}</p>
+                <p>{s1} {s2} {s3}</p>
+                <p>{isWinner ? "Winner!" : "Looser!"}</p>
             </div>
          );
     }
