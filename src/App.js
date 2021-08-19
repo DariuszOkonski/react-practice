0d3146e229ab5398dcdb4c23ce19d3a011fa8973
Dariusz Okonski <darek200180@gmail.com>
2021-08-20 00:13:38 +0100

    working on Machines

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Machine from './slotMachines/Machine';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <h1>Slot Machines!</h1>
 
         <Machine 
           s1="x"
@@ -23,7 +23,6 @@ function App() {
           s3="z"
         />
 
-      </header>
     </div>
   );
 }
