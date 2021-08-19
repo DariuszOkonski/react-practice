02c188ba2a40a35fcf3267692e6f396dbaffd584
Dariusz Okonski <darek200180@gmail.com>
2021-08-20 00:27:32 +0100

    working on Friend component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import './App.css';
-import Machine from './slotMachines/Machine';
+import Friend from './Friend';
 
 function App() {
   return (
     <div className="App">
-        <h1>Slot Machines!</h1>
 
-        <Machine 
-          s1="x"
-          s2="y"
-          s3="z"
-        />
-        <Machine 
-          s1="z"
-          s2="z"
-          s3="z"
-        />
-        
-        <Machine 
-          s1="x"
-          s2="x"
-          s3="z"
-        />
+      <Friend
+        name="Elton"
+        hobbies={['Piano', 'Singing', 'Dancing']}
+      />        
 
     </div>
   );
---
+++ src/Friend.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Friend extends Component {
+    render() { 
+        const { name, hobbies } = this.props;
+        return ( 
+            <div>
+                <h1>{name}</h1>
+                <ul>
+                    {
+                        hobbies.map(h => <li key={h}>{h}</li>)
+                    }
+                </ul>
+            </div>
+         );
+    }
+}
+ 
+export default Friend;
