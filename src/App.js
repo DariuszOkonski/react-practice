b583957238eef4c2d66757ca455e4b4644cf2e19
Dariusz Okonski <darek200180@gmail.com>
2021-08-23 23:57:12 +0100

    changes at app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import PokemondexMain from './Pokemondex/PokemondexMain';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <PokemondexMain />
     </div>
   );
 }
