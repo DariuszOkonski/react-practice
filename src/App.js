a5a8ca1ed168e3319b9c9d5d236fa55d31becb9e
Dariusz Okonski <darek200180@gmail.com>
2021-08-23 23:21:43 +0100

    remove Machines

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import Machine from './additionalComponents/Machine';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-    
-      <Machine 
-        s1="x"
-        s2="z"
-        s3="x"
-      />      
-
     </div>
   );
 }
