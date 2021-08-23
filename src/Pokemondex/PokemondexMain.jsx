23c8aa65fe230dcd122736dea8dc30fdb4a73625
Dariusz Okonski <darek200180@gmail.com>
2021-08-23 23:56:16 +0100

    build pokemondex

---
+++ src/Pokemondex/PokemondexMain.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PokemondexMain extends Component {
+    render() { 
+        return ( 
+            <div>
+                <h1>Pok</h1>
+            </div>
+         );
+    }
+}
+ 
+export default PokemondexMain;
