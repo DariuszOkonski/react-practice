3cd843c33339a50f66b709d0edb8fede274399d6
Dariusz Okonski <darek200180@gmail.com>
2021-08-25 23:30:31 +0100

    style pokegame

--- src/Pokemondex/Pokecard.jsx
+++ src/Pokemondex/Pokecard.jsx
@@ -14,7 +14,9 @@ class Pokecard extends Component {
         return ( 
             <div className="Pokecard">
                 <h1 className="Pokecard-title">{this.props.name}</h1>
-                <img src={imgSrc} alt={this.props.name} />
+                <div className="Pokecard-image">
+                    <img src={imgSrc} alt={this.props.name} />                    
+                </div>
                 <div className="Pokecard-data">Type: {this.props.type}</div>
                 <div className="Pokecard-data">Exp: {this.props.exp}</div>
             </div>
--- src/Pokemondex/Pokedex.jsx
+++ src/Pokemondex/Pokedex.jsx
@@ -5,11 +5,17 @@ import './Pokedex.css'
 
 class Pokedex extends Component {
     render() { 
+        let title;
+        if(this.props.isWinner) {
+            title = <h1 className="Pokedex-winner">Winning Hand</h1>
+        } else {
+            title = <h1 className="Pokedex-looser">Loosing Hand</h1>
+        }
+
         return ( 
             <div className="Pokedex">
-                <h1>Pokedex</h1>
-                <p>Total Experience: {this.props.exp}</p>
-                <p>{this.props.isWinner ? "Winner" : "Looser"}</p>
+                <h4>Total Experience: {this.props.exp}</h4>
+                {title}
                 <div className="Pokedex-cards">                    
                     {
                         this.props.pokemon.map(p => 
