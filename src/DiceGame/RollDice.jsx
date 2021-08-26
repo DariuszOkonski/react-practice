d7a98e425eae0a060065f18ac79531ce2d4a5da6
Dariusz Okonski <darek200180@gmail.com>
2021-08-27 00:55:52 +0100

    add some css

---
+++ src/DiceGame/RollDice.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import Die from './Die';
+import './RollDice.css';
+
+class RollDice extends Component {
+    static defaultProps = {
+        sides: ['one', 'two', 'three', 'four', 'five', 'six']
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            die1: 'one',
+            die2: 'two'
+        }
+        this.roll = this.roll.bind(this);
+    }
+    
+    roll() {
+        const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+        const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+
+        this.setState({
+            die1: newDie1,
+            die2: newDie2
+        })
+    }
+
+    render() { 
+        return ( 
+            <div className="RollDice">
+                <div className="RollDice-container">
+                    <Die face={this.state.die1} />
+                    <Die face={this.state.die2} />
+                </div>
+
+                <button onClick={this.roll}>Roll Dice!</button>
+            </div>
+         );
+    }
+}
+ 
+export default RollDice;
