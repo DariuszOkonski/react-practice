1bfc4c9a4d1100c26728b83beca79246b97c5188
Dariusz Okonski <darek200180@gmail.com>
2021-08-27 00:33:47 +0100

    import fontawesome to index.html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import Game from './State/Game';
+import Die from './DiceGame/Die';
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello</h1>
-        <Game />
+
+        <Die face="five" />
+        <Die face="six" />
+        <Die face="four" />
+
     </div>
   );
 }
