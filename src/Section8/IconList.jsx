f4d93a84d926cfdf3e993e745fe70c326cdc7f02
Dariusz Okonski <darek200180@gmail.com>
2021-08-27 23:48:20 +0100

    add class IconList

---
+++ src/Section8/IconList.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+class IconList extends Component {
+    static defaultProps = {
+        options: ['angry', 'anchor', 'archive', 'at', 'archway', 'baby', 'bell', 'bolt', 'bone', 'car', 'city', 'cloud', 'couch']
+    }
+
+    constructor(props) {
+        super(props);
+        this.state ={
+            icons: []
+        }
+        this.addIcon = this.addIcon.bind(this);
+    }
+
+    addIcon() {
+        console.log('add icon')
+        let idx = Math.floor(Math.random() * this.props.options.length);
+        let newIcon = this.props.options[idx];
+        let icons = [...this.state.icons, newIcon]
+        console.log(icons)
+
+        this.setState({
+            icons
+        })
+    }
+
+    render() { 
+        const icons = this.state.icons.map(i => <i key={Math.random()} className={`fas fa-${i}`} />)
+
+        return ( 
+            <div>
+                <h1>Icons: {icons}</h1>
+                <button onClick={this.addIcon}>Add New Icon</button>
+            </div>
+         );
+    }
+}
+ 
+export default IconList;
