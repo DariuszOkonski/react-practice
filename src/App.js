83bbf1c55d833da0a8ffe53354b3615d6aeb4838
Dariusz Okonski <darek200180@gmail.com>
2021-09-07 23:14:19 +0100

    working with useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 
 const App = () => {  
+  const [counter, setCounter] = useState(0);
+  const [isActive, setIsActive] = useState(true);
+
+  const handler = () => setCounter(prevState => prevState + 1);
+  const toggleVisibilityCounter = () => setIsActive(prevValue => !prevValue);
+
+  const counterComponent = isActive
+    ? <Counter2 />
+    : null;
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <button onClick={toggleVisibilityCounter}>
+        Pokaż/ukryj komponent
+      </button>
+
+      {counterComponent}
     </div>
   
   );
+}
+
+const Counter2 = () => {
+  const [counter, setCounter] = useState(0);
+  
+  const handleMouseMove = (event) => {
+    setCounter(event.clientX)
+  }
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove)
+    
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    }
+  }, [])
+
+
+  return (
+    <div>
+      <p>{counter}</p>
+      {/* <p>{rerenderCounter}</p> */}
+    </div>
+  )
 }
 
 export default App;
