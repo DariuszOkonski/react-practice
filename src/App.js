02a5a3882d3cbf449a5548347bf4b90848e3b3fd
Dariusz Okonski <darek200180@gmail.com>
2021-09-07 22:27:08 +0100

    working with hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
-import React, { useState } from 'react';
-import Counter from './aaaSamuraj/Counter';
+import React from 'react';
 import './App.css';
 
 
-const App = () => {
-  
+const App = () => {  
   return (
     <div className="App">
-      <Counter result={0} />
+      <h1>Hello world</h1>
     </div>
+  
   );
 }
 
