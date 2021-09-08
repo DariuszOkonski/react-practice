4ebf96b33cec8d7a066e3b9a84a7b9ced8fdd69d
Dariusz Okonski <darek200180@gmail.com>
2021-09-09 00:00:31 +0100

    working on contexapi

--- src/App.js
+++ src/App.js
@@ -1,52 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import AppClass from './aaaSamuraj/AppClass';
 import './App.css';
 
 
 const App = () => {  
-  const [counter, setCounter] = useState(0);
-  const [isActive, setIsActive] = useState(true);
-
-  const handler = () => setCounter(prevState => prevState + 1);
-  const toggleVisibilityCounter = () => setIsActive(prevValue => !prevValue);
-
-  const counterComponent = isActive
-    ? <Counter2 />
-    : null;
-
   return (
     <div className="App">
-      <button onClick={toggleVisibilityCounter}>
-        Pokaż/ukryj komponent
-      </button>
-
-      {counterComponent}
+      <AppClass />
     </div>
   
   );
 }
 
-const Counter2 = () => {
-  const [counter, setCounter] = useState(0);
-  
-  const handleMouseMove = (event) => {
-    setCounter(event.clientX)
-  }
-
-  useEffect(() => {
-    window.addEventListener('mousemove', handleMouseMove)
-    
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    }
-  }, [])
-
-
-  return (
-    <div>
-      <p>{counter}</p>
-      {/* <p>{rerenderCounter}</p> */}
-    </div>
-  )
-}
 
 export default App;
---
+++ src/aaaSamuraj/AppClass.jsx
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react'
+
+class AppClass extends PureComponent {
+    
+    
+    render() { 
+        return (
+            <div>
+                <h1>AppClass</h1>
+            </div>
+        );
+    }
+}
+ 
+export default AppClass;
