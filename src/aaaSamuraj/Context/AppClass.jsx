19bdd257051479fcc6559d5ee22ae96e6c5ae78d
Dariusz Okonski <darek200180@gmail.com>
2021-09-11 00:10:34 +0100

    change AppClass to functional component

--- src/aaaSamuraj/Context/AppClass.jsx
+++ src/aaaSamuraj/Context/AppClass.jsx
@@ -1,50 +1,39 @@
-import React, { PureComponent } from 'react'
+import React, { useState } from 'react'
 import { AppContext, defaultObject } from './AppContext';
 
 import Button from './Button';
 import UserInfo from './UserInfo';
 
-class AppClass extends PureComponent {
-    state = {
-        isUserLogged: defaultObject.isUserLogged,
-        isUserAdult: true,
-    }
+const AppClass = () => {    
+    const [isUserLogged, setIsUserLogged] = useState(defaultObject.isUserLogged);
+    const [isUserAdult, setIsUserAdult] = useState(true);
     
-    handleToggleStateIsLogged = () => {
-        this.setState(prevState => ({
-            isUserLogged: !prevState.isUserLogged
-        }));
-    }
 
-    handleIsUserAdult = () => {
-        this.setState((prevState) => ({
-            isUserAdult: !prevState.isUserAdult
-        }))
-    }
+    const handleToggleStateIsLogged = () => setIsUserLogged(!isUserLogged);
 
-    render() { 
-        return (
-            <div>
-                <AppContext.Provider value={{
-                    isUserLogged: this.state.isUserLogged,
-                    isUserAdult: this.state.isUserAdult,
-                    toggleLoggedState: this.handleToggleStateIsLogged
-                }}>
-                    <UserInfo />
-                    <Button />
-                </AppContext.Provider>
-                
-                <AppContext.Provider value={{
-                    isUserLogged: this.state.isUserAdult,
-                    toggleLoggedState: this.handleIsUserAdult
-                }}>
-                    <UserInfo />
-                    <Button />
-                </AppContext.Provider>
-            </div>
-                
-        );
-    }
+    const handleIsUserAdult = () => setIsUserAdult(!isUserAdult);
+
+    return (
+        <div>
+            <AppContext.Provider value={{
+                isUserLogged: isUserLogged,
+                isUserAdult: isUserAdult,
+                toggleLoggedState: handleToggleStateIsLogged
+            }}>
+                <UserInfo />
+                <Button />
+            </AppContext.Provider>
+            
+            <AppContext.Provider value={{
+                isUserLogged: isUserAdult,
+                toggleLoggedState: handleIsUserAdult
+            }}>
+                <UserInfo />
+                <Button />
+            </AppContext.Provider>
+        </div>
+            
+    );
 }
  
 export default AppClass;
